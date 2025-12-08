168a2e79f0450b9251a8b9a92fd9d416f08c4a59
samsul alam <samsul7718@gmail.com>
2025-12-08 08:56:41 +0530

    added proper url for product

--- src/component/HomePage.jsx
+++ src/component/HomePage.jsx
@@ -10,7 +10,7 @@ const HomePage = () => {
     // const {addToCart}=useCart()
 
     useEffect(()=>{
-        fetch("http://localhost:3000/api/product")
+        fetch(`${import.meta.env.VITE_API_URL}/api/product`)
         .then(res=>res.json())
         .then(data=>setProducts(data))
         .catch(err=>console.log(err));
